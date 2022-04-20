6198b007eeba1b29051827be8aa5ecb67a219de0
citadel <unitedmilitaryum@gmail.com>
2022-04-20 10:17:12 -0700

    first commit for WockyFX Java (nothing functional yet!)

---
+++ Wocky4J/src/main/java/me/c1tad31/wocky4j/Main.java
@@ -0,0 +1,8 @@
+package me.c1tad31.wocky4j;
+
+public class Main {
+    public static void main(String[] args) {
+        // For Debugging purposes only (not for production use)
+        // System.out.println("Hello World!");
+    }
+}
