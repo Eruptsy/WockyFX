c43336479e8e822fa1a607ed9f8cefbc46e5cc2a
citadel <unitedmilitaryum@gmail.com>
2022-04-20 11:55:32 -0700

    updated WockyFX.java (nothing functional yet!)

--- Wocky4J/src/main/java/me/c1tad31/wocky4j/WockyFX.java
+++ Wocky4J/src/main/java/me/c1tad31/wocky4j/WockyFX.java
@@ -3,7 +3,17 @@ package me.c1tad31.wocky4j;
 import me.c1tad31.wocky4j.enums.FileRanks;
 import me.c1tad31.wocky4j.enums.FileTypes;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class WockyFX {
     private ServerSocket socket;
@@ -21,8 +31,20 @@ public class WockyFX {
     // private final Map<String, Integer> perms = new HashMap<>();
     // private final String[] dataTypes = {"int", "for"};
 
-    public void wfxFile(String filePath, FileTypes fileTypes1) {
-        // Logic for wfx files
+    public void wfxFile(String filePath, FileTypes fileTypes1) throws IOException, URISyntaxException {
+        Path path = Paths.get(getClass().getClassLoader().getResource(filePath).toURI());
+
+        Stream<String> lines = Files.lines(path);
+        String data = lines.collect(Collectors.joining("\n"));
+
+        if (data.equals("")) {
+            System.err.println("[x] Error, This WFX files contains no data!");
+            return;
+        }
+
+        file = filePath;
+        fileData = data;
+        fileLines = data.split("\n");
     }
 
     public void setCmdInfo(String fcmd, String cmd, String[] args) {
@@ -37,7 +59,11 @@ public class WockyFX {
     }
 
     public void parseWfx() {
-        // Check for perm keyword and remove
+        if (fileTypes.equals(FileTypes.WFX)) {
+            parsePerms(fileLines[0]);
+        }
+
+        // Check for max arguments
 
     }
 
