9998b3fdede49d5d1a273bf8a0b53743b5e881ea
citadel <unitedmilitaryum@gmail.com>
2022-04-23 09:29:34 -0700

    updated WockyFX.java

--- Wocky4J/src/main/java/me/c1tad31/wocky4j/Main.java
+++ Wocky4J/src/main/java/me/c1tad31/wocky4j/Main.java
@@ -1,8 +1,25 @@
 package me.c1tad31.wocky4j;
 
+import me.c1tad31.wocky4j.enums.FileTypes;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException {
         // For Debugging purposes only (not for production use)
         // System.out.println("Hello World!");
+
+        WockyFX wockyFX = new WockyFX();
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print(">>> ");
+            String cmdIn = scanner.nextLine();
+
+            wockyFX.setFile(cmdIn, FileTypes.WFX);
+            wockyFX.parseWfx();
+        }
     }
 }
---
+++ Wocky4J/src/main/java/me/c1tad31/wocky4j/enums/DataTypes.java
@@ -0,0 +1,7 @@
+package me.c1tad31.wocky4j.enums;
+
+public enum DataTypes {
+    STR,
+    INTEGER,
+    FUNC
+}
